ab14ba0cd0b0b39b54973ed7e8a1154ae6347bc5
Jaswanth Chennu <chennujaswanth7@gmail.com>
2026-07-24 10:11:06 +0530

    Day 2

---
+++ p7.py
@@ -0,0 +1,15 @@
+class Father:
+    def skills1(self):
+        print("Driving")
+
+class Mother:
+    def skills2(self):
+        print("Cooking")
+
+class Child(Father, Mother):
+    pass
+
+c = Child()
+
+c.skills1()
+c.skills2()
